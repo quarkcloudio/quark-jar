0a0b93fc104c33bca80469b8e6632ae214b2bbde
tangtanglove <dai_hang_love@126.com>
2024-01-21 17:20:42 +0800

    feat: first commit

---
+++ src/main/java/io/quarkcloud/quarkjar/QuarkjarApplication.java
@@ -0,0 +1,13 @@
+package io.quarkcloud.quarkjar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuarkjarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuarkjarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/quarkcloud/quarkjar/QuarkjarApplicationTests.java
@@ -0,0 +1,13 @@
+package io.quarkcloud.quarkjar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuarkjarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
