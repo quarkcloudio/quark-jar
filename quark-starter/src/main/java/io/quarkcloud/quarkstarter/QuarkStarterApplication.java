64204b29a997f5873977bc080f10763d5bc57a6a
tangtanglove <dai_hang_love@126.com>
2024-04-23 18:23:18 +0800

    chore: wip

--- quark-starter/src/main/java/io/quarkcloud/quarkstarter/QuarkStarterApplication.java
+++ quark-starter/src/main/java/io/quarkcloud/quarkstarter/QuarkStarterApplication.java
@@ -7,9 +7,9 @@ import io.quarkcloud.quarkadmin.annotation.QuarkAdmin;
 
 @SpringBootApplication(scanBasePackages = {"io.quarkcloud.quarkstarter","io.quarkcloud.quarkadmin"})
 @MapperScan(basePackages = {"io.quarkcloud.quarkadmin.mapper"})
+@QuarkAdmin({"abcd"})
 public class QuarkStarterApplication {
 
-    @QuarkAdmin({"abcd"})
     public static void main(String[] args) {
         SpringApplication.run(QuarkStarterApplication.class, args);
     }
