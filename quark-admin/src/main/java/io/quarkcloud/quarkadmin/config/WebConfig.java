e7a0f7bc359f6c26e1a8793cb47eccb293d79f8b
tangtanglove <dai_hang_love@126.com>
2024-05-07 11:37:42 +0800

    chore: 新增配置文件

---
+++ quark-admin/src/main/java/io/quarkcloud/quarkadmin/config/WebConfig.java
@@ -0,0 +1,20 @@
+package io.quarkcloud.quarkadmin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+ 
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    @SuppressWarnings("null")
+    public void addViewControllers(ViewControllerRegistry registry) {
+
+        // "/admin"重定向到"/admin/"
+        registry.addViewController("/admin").setViewName("redirect:/admin/");
+
+        // 添加二级目录的index.html映射
+        registry.addViewController("/admin/").setViewName("forward:/admin/index.html");
+    }
+}
