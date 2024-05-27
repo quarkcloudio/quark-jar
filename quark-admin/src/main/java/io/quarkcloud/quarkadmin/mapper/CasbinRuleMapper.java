f212aa4ad41de4ac218520f8e7f2bf44a1f5ce10
tangtanglove <dai_hang_love@126.com>
2024-05-27 19:04:45 +0800

    chore: wip

---
+++ quark-admin/src/main/java/io/quarkcloud/quarkadmin/mapper/CasbinRuleMapper.java
@@ -0,0 +1,10 @@
+package io.quarkcloud.quarkadmin.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import io.quarkcloud.quarkadmin.entity.CasbinRule;
+
+@Mapper
+public interface CasbinRuleMapper extends BaseMapper<CasbinRule> {
+
+}
