2a50421718f0353f984a627a2075f33cebd49d5b
tangtanglove <dai_hang_love@126.com>
2024-05-30 13:53:56 +0800

    chore: wip

--- quark-admin/src/main/java/io/quarkcloud/quarkadmin/service/MenuService.java
+++ quark-admin/src/main/java/io/quarkcloud/quarkadmin/service/MenuService.java
@@ -1,6 +1,11 @@
 package io.quarkcloud.quarkadmin.service;
 
-public interface MenuService {
+import java.util.List;
+import com.baomidou.mybatisplus.extension.service.IService;
+import io.quarkcloud.quarkadmin.entity.Menu;
 
-    
+public interface MenuService extends IService<Menu> {
+
+    // 根据用户id获取菜单列表
+    public List<Menu> getListByUserId(String userId);
 }
--- quark-admin/src/main/java/io/quarkcloud/quarkadmin/service/impl/MenuServiceImpl.java
+++ quark-admin/src/main/java/io/quarkcloud/quarkadmin/service/impl/MenuServiceImpl.java
@@ -1,5 +1,16 @@
 package io.quarkcloud.quarkadmin.service.impl;
 
-public class MenuServiceImpl {
+import java.util.List;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import io.quarkcloud.quarkadmin.entity.Menu;
+import io.quarkcloud.quarkadmin.mapper.MenuMapper;
+import io.quarkcloud.quarkadmin.service.MenuService;
+
+public class MenuServiceImpl extends ServiceImpl<MenuMapper, Menu> implements MenuService  {
     
+    // 根据用户id获取菜单列表
+    public List<Menu> getListByUserId(String userId) {
+        return null;
+    }
 }
