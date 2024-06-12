bae672ea09b8ab907bb4633dd747cdcff1edbd48
tangtanglove <dai_hang_love@126.com>
2024-06-12 13:32:25 +0800

    chore: wip

--- quark-starter/src/main/java/io/quarkcloud/quarkstarter/service/admin/dashboard/Index.java
+++ quark-starter/src/main/java/io/quarkcloud/quarkstarter/service/admin/dashboard/Index.java
@@ -9,6 +9,7 @@ import io.quarkcloud.quarkadmin.template.Dashboard;
 import io.quarkcloud.quarkadmin.template.metrics.Metrics;
 import io.quarkcloud.quarkcore.service.Context;
 import io.quarkcloud.quarkstarter.service.admin.metric.SystemInfo;
+import io.quarkcloud.quarkstarter.service.admin.metric.TeamInfo;
 
 @Component(value = "dashboardIndex")
 public class Index extends Dashboard {
@@ -21,7 +22,8 @@ public class Index extends Dashboard {
     // 内容
     public List<Metrics> cards(Context context) {
         return Arrays.asList(
-            new SystemInfo()
+            new SystemInfo(),
+            new TeamInfo()
         );
     }
 }
