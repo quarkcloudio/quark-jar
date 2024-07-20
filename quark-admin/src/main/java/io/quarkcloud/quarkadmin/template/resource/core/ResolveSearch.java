590f91ff7e9dd4d67ae93d5b3edab5759618d623
tangtanglove <dai_hang_love@126.com>
2024-07-20 17:51:14 +0800

    chore: wip

--- quark-admin/src/main/java/io/quarkcloud/quarkadmin/template/resource/core/ResolveSearch.java
+++ quark-admin/src/main/java/io/quarkcloud/quarkadmin/template/resource/core/ResolveSearch.java
@@ -1,5 +1,23 @@
 package io.quarkcloud.quarkadmin.template.resource.core;
 
-public class ResolveSearch {
+import java.util.List;
 
+import io.quarkcloud.quarkcore.service.Context;
+
+public class ResolveSearch {
+    
+    // searches
+    public List<Object> searches;
+
+    // context
+    public Context context;
+
+    // 构造函数
+    public ResolveSearch() {}
+
+    // 构造函数
+    public ResolveSearch(List<Object> searches, Context context) {
+        this.searches = searches;
+        this.context = context;
+    }
 }
