1f79fd78b8aea8fcf0e3070125485391c9fc39a4
tangtanglove <dai_hang_love@126.com>
2024-07-22 16:21:57 +0800

    chore: wip

--- quark-admin/src/main/java/io/quarkcloud/quarkadmin/template/resource/core/ResolveSearch.java
+++ quark-admin/src/main/java/io/quarkcloud/quarkadmin/template/resource/core/ResolveSearch.java
@@ -2,22 +2,143 @@ package io.quarkcloud.quarkadmin.template.resource.core;
 
 import java.util.List;
 
+import io.quarkcloud.quarkadmin.component.form.Field;
+import io.quarkcloud.quarkadmin.component.form.fields.Cascader;
+import io.quarkcloud.quarkadmin.component.form.fields.Radio;
+import io.quarkcloud.quarkadmin.component.form.fields.SelectField;
+import io.quarkcloud.quarkadmin.component.form.fields.TreeSelect;
+import io.quarkcloud.quarkadmin.component.table.Search;
 import io.quarkcloud.quarkcore.service.Context;
 
-public class ResolveSearch {
+public class ResolveSearch<T> {
     
     // searches
     public List<Object> searches;
 
-    // context
+    // 上下文
     public Context context;
 
-    // 构造函数
-    public ResolveSearch() {}
+    // 是否包含导出功能
+    public boolean withExport;
 
     // 构造函数
     public ResolveSearch(List<Object> searches, Context context) {
         this.searches = searches;
         this.context = context;
+    }
+
+    // 列表行为
+    public ResolveSearch<T> setWithExport(boolean withExport) {
+        this.withExport = withExport;
+        return this;
+    }
+
+    // 列表页搜索栏
+    @SuppressWarnings("unchecked")
+    public Object indexSearches(Context ctx) {
+
+        // 初始化搜索组件
+        Search searchComponent = new Search();
+        if (withExport) {
+            String resource = ctx.getPathVariable("resource");
+            String[] paths = ctx.getRequestMapping();
+            for (String path : paths) {
+                searchComponent.setExportText("导出").setExportApi(path.replace(":resource", resource));
+            }
+        }
+        if (searches == null) {
+            return searchComponent;
+        }
+
+        // 解析搜索项
+        for (Object v : searches) {
+
+            // 搜索项实例
+            Object field = new Object();
+
+            io.quarkcloud.quarkadmin.template.resource.Search<T> search = (io.quarkcloud.quarkadmin.template.resource.Search<T>) v;
+
+            // 获取组件名称
+            String component = search.getComponent();
+
+            // 获取label标签文本
+            String label = search.getName();
+
+            // 获取字段名，支持数组
+            String name = search.getColumn(v);
+
+            // 获取接口
+            String api = search.getApi();
+
+            // 获取属性
+            Object options = search.options(ctx);
+
+            // 根据组件类型构建组件
+            switch (component) {
+                case "textField":
+                    field = Field.
+                        text(name, label).
+                        setWidth(null);
+                    break;
+                case "selectField":
+                    field = Field.
+                        select(name, label).
+                        setWidth(null).
+                        setOptions((List<SelectField.Option>) options);
+                    break;
+                case "radioField":
+                    field = Field.
+                        radio(name, label).
+                        setOptions((List<Radio.Option>) options).
+                        setOptionType("button").
+                        setButtonStyle("solid");
+                    break;
+                case "multipleSelectField":
+                    field = Field.
+                        select(name, label).
+                        setMode("multiple").
+                        setWidth(null).
+                        setOptions((List<SelectField.Option>) options);
+                    break;
+                case "dateField":
+                    field = Field.
+                        date(name, label).
+                        setWidth(null);
+                    break;
+                case "datetimeField":
+                    field = Field.
+                        datetime(name, label).
+                        setWidth(null);
+                    break;
+                case "dateRangeField":
+                    field = Field.
+                        dateRange(name, label).
+                        setWidth(null);
+                    break;
+                case "datetimeRangeField":
+                    field = Field.
+                        datetimeRange(name, label).
+                        setWidth(null);
+                    break;
+                case "cascaderField":
+                    field = Field.
+                        cascader(name, label).
+                        setApi(api).
+                        setWidth(null).
+                        setOptions((List<Cascader.Option>) options);
+                    break;
+                case "treeSelectField":
+                    field = Field.
+                        treeSelect(name, label).
+                        setWidth(null).
+                        setTreeData((List<TreeSelect.TreeData>) options);
+                    break;
+            }
+            if (field != null) {
+                searchComponent.setItems(field);
+            }
+        }
+
+        return searchComponent;
     }
 }
--- quark-admin/src/main/java/io/quarkcloud/quarkadmin/template/resource/impl/ResourceImpl.java
+++ quark-admin/src/main/java/io/quarkcloud/quarkadmin/template/resource/impl/ResourceImpl.java
@@ -26,6 +26,7 @@ import io.quarkcloud.quarkadmin.template.resource.Action;
 import io.quarkcloud.quarkadmin.template.resource.Resource;
 import io.quarkcloud.quarkadmin.template.resource.core.ResolveAction;
 import io.quarkcloud.quarkadmin.template.resource.core.ResolveField;
+import io.quarkcloud.quarkadmin.template.resource.core.ResolveSearch;
 
 public class ResourceImpl<M extends ResourceMapper<T>, T> implements Resource<T> {
 
@@ -327,121 +328,6 @@ public class ResourceImpl<M extends ResourceMapper<T>, T> implements Resource<T>
         return null;
     }
 
-    // 列表页搜索栏
-    @SuppressWarnings("unchecked")
-    public Object indexSearches(Context ctx) {
-
-        // 获取搜索项
-        List<Object> searches = this.searches(ctx);
-
-        // 初始化搜索组件
-        Search searchComponent = new Search();
-
-        // 是否包含导出功能
-        boolean withExport = this.isWithExport();
-        if (withExport) {
-            String resource = ctx.getPathVariable("resource");
-            String[] paths = ctx.getRequestMapping();
-            for (String path : paths) {
-                searchComponent.setExportText("导出").setExportApi(path.replace(":resource", resource));
-            }
-        }
-        if (searches == null) {
-            return searchComponent;
-        }
-
-        // 解析搜索项
-        for (Object v : searches) {
-
-            // 搜索项实例
-            Object field = new Object();
-
-            io.quarkcloud.quarkadmin.template.resource.Search<T> search = (io.quarkcloud.quarkadmin.template.resource.Search<T>) v;
-
-            // 获取组件名称
-            String component = search.getComponent();
-
-            // 获取label标签文本
-            String label = search.getName();
-
-            // 获取字段名，支持数组
-            String name = search.getColumn(v);
-
-            // 获取接口
-            String api = search.getApi();
-
-            // 获取属性
-            Object options = search.options(ctx);
-
-            // 根据组件类型构建组件
-            switch (component) {
-                case "textField":
-                    field = Field.
-                        text(name, label).
-                        setWidth(null);
-                    break;
-                case "selectField":
-                    field = Field.
-                        select(name, label).
-                        setWidth(null).
-                        setOptions((List<SelectField.Option>) options);
-                    break;
-                case "radioField":
-                    field = Field.
-                        radio(name, label).
-                        setOptions((List<Radio.Option>) options).
-                        setOptionType("button").
-                        setButtonStyle("solid");
-                    break;
-                case "multipleSelectField":
-                    field = Field.
-                        select(name, label).
-                        setMode("multiple").
-                        setWidth(null).
-                        setOptions((List<SelectField.Option>) options);
-                    break;
-                case "dateField":
-                    field = Field.
-                        date(name, label).
-                        setWidth(null);
-                    break;
-                case "datetimeField":
-                    field = Field.
-                        datetime(name, label).
-                        setWidth(null);
-                    break;
-                case "dateRangeField":
-                    field = Field.
-                        dateRange(name, label).
-                        setWidth(null);
-                    break;
-                case "datetimeRangeField":
-                    field = Field.
-                        datetimeRange(name, label).
-                        setWidth(null);
-                    break;
-                case "cascaderField":
-                    field = Field.
-                        cascader(name, label).
-                        setApi(api).
-                        setWidth(null).
-                        setOptions((List<Cascader.Option>) options);
-                    break;
-                case "treeSelectField":
-                    field = Field.
-                        treeSelect(name, label).
-                        setWidth(null).
-                        setTreeData((List<TreeSelect.TreeData>) options);
-                    break;
-            }
-            if (field != null) {
-                searchComponent.setItems(field);
-            }
-        }
-
-        return searchComponent;
-    }
-
     // 列表页标题
     public String indexTableTitle(Context ctx) {
         return this.getTitle() + this.getTableTitleSuffix();
@@ -472,7 +358,9 @@ public class ResourceImpl<M extends ResourceMapper<T>, T> implements Resource<T>
         Object indexTableAlertActions = indexTableAlertActions(ctx);
 
         // 列表页搜索栏
-        Object indexSearches = indexSearches(ctx);
+        Object indexSearches = new ResolveSearch<T>(this.searches(ctx), ctx).
+            setWithExport(this.isWithExport()).
+            indexSearches(ctx);
 
         // 表格组件
         table.setPolling(tablePolling)
