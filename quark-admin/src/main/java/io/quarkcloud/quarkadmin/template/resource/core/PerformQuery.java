0cefa381312dd64b4469d1275ef8469ad88d4868
tangtanglove <dai_hang_love@126.com>
2024-07-27 11:27:39 +0800

    chore: wip

--- quark-admin/src/main/java/io/quarkcloud/quarkadmin/template/resource/core/PerformQuery.java
+++ quark-admin/src/main/java/io/quarkcloud/quarkadmin/template/resource/core/PerformQuery.java
@@ -1,14 +1,24 @@
 package io.quarkcloud.quarkadmin.template.resource.core;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+
 import io.quarkcloud.quarkcore.service.Context;
 
-public class PerformQuery {
+public class PerformQuery<T> {
 
     // context
     public Context context;
 
+    // 查询条件
+    protected QueryWrapper<T> queryWrapper;
+
     // 构造函数
     public PerformQuery(Context context) {
         this.context = context;
+    }
+
+    // 构建查询条件
+    public QueryWrapper<T> buildIndexQuery() {
+        return null;
     }
 }
