d70c1516aee0291d88ea3a475558fea123fe9a0e
tangtanglove <dai_hang_love@126.com>
2024-07-27 17:07:57 +0800

    chore: 优化分页条件

--- quark-admin/src/main/java/io/quarkcloud/quarkadmin/service/impl/ResourceServiceImpl.java
+++ quark-admin/src/main/java/io/quarkcloud/quarkadmin/service/impl/ResourceServiceImpl.java
@@ -1,10 +1,14 @@
 package io.quarkcloud.quarkadmin.service.impl;
 
 import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.quarkcloud.quarkadmin.mapper.ResourceMapper;
 import io.quarkcloud.quarkadmin.service.ResourceService;
@@ -51,12 +55,27 @@ public class ResourceServiceImpl<M extends ResourceMapper<T>, T> implements Reso
     }
 
     // 获取分页数据
+    @SuppressWarnings("unchecked")
     public IPage<T> page(long pageSize) {
         QueryWrapper<T> queryWrapper = new PerformQuery<T>(context, this.queryWrapper).setSearches(searches).buildIndexQuery();
         long currentPage = 1;
-        String getPage = context.getRequest().getParameter("page");
-        if (getPage!=null) {
-            currentPage = Long.parseLong(getPage);
+        String searchParam = context.getParameter("search");
+        if (searchParam == null || searchParam == "") {
+            ObjectMapper mapper = new ObjectMapper();
+            Map<String, Object> map = null;
+            try {
+                map = mapper.readValue(searchParam, Map.class);
+            } catch (JsonProcessingException e) {
+                currentPage = 1;
+            }
+            if (map!=null) {
+                if (map.get("page")!=null) {
+                    currentPage = Long.parseLong((String)map.get("page"));
+                }
+                if (map.get("pageSize")!=null) {
+                    pageSize = Long.parseLong((String)map.get("pageSize"));
+                }
+            }
         }
 
         // 构建分页
