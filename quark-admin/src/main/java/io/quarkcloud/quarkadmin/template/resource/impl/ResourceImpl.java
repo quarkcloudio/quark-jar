25d1e10ccc1b789886dc9fb471aa908f6e52f5e3
tangtanglove <dai_hang_love@126.com>
2024-08-29 08:22:11 +0800

    chore: wip

--- quark-admin/src/main/java/io/quarkcloud/quarkadmin/template/resource/impl/ResourceImpl.java
+++ quark-admin/src/main/java/io/quarkcloud/quarkadmin/template/resource/impl/ResourceImpl.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.ibatis.transaction.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
