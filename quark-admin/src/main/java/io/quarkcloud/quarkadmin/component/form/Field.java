9759e0fb695b6d01dcbdf201d3724f2e3445bec7
tangtanglove <dai_hang_love@126.com>
2024-08-30 16:21:34 +0800

    chore: wip

--- quark-admin/src/main/java/io/quarkcloud/quarkadmin/component/form/Field.java
+++ quark-admin/src/main/java/io/quarkcloud/quarkadmin/component/form/Field.java
@@ -52,12 +52,20 @@ public class Field {
         return new Cascader().setName(name).setLabel(label);
     }
 
+    public static Cascader cascader(String name, String label, Closure callback) {
+        return new Cascader().setName(name).setLabel(label).setCallback(callback);
+    }
+
     public static Checkbox checkbox(String name) {
         return new Checkbox().setName(name);
     }
 
     public static Checkbox checkbox(String name, String label) {
         return new Checkbox().setName(name).setLabel(label);
+    }
+
+    public static Checkbox checkbox(String name, String label, Closure callback) {
+        return new Checkbox().setName(name).setLabel(label).setCallback(callback);
     }
 
     public static Compact compact(String name) {
@@ -68,12 +76,20 @@ public class Field {
         return new Compact().setName(name).setLabel(label);
     }
 
+    public static Compact compact(String name, String label, Closure callback) {
+        return new Compact().setName(name).setLabel(label).setCallback(callback);
+    }
+
     public static Date date(String name) {
         return new Date().setName(name);
     }
 
     public static Date date(String name, String label) {
         return new Date().setName(name).setLabel(label);
+    }
+
+    public static Date date(String name, String label, Closure callback) {
+        return new Date().setName(name).setLabel(label).setCallback(callback);
     }
 
     public static DateRange dateRange(String name) {
@@ -84,12 +100,20 @@ public class Field {
         return new DateRange().setName(name).setLabel(label);
     }
 
+    public static DateRange dateRange(String name, String label, Closure callback) {
+        return new DateRange().setName(name).setLabel(label).setCallback(callback);
+    }
+
     public static Datetime datetime(String name) {
         return new Datetime().setName(name);
     }
 
     public static Datetime datetime(String name, String label) {
         return new Datetime().setName(name).setLabel(label);
+    }
+
+    public static Datetime datetime(String name, String label, Closure callback) {
+        return new Datetime().setName(name).setLabel(label).setCallback(callback);
     }
 
     public static DatetimeRange datetimeRange(String name) {
@@ -100,12 +124,20 @@ public class Field {
         return new DatetimeRange().setName(name).setLabel(label);
     }
 
+    public static DatetimeRange datetimeRange(String name, String label, Closure callback) {
+        return new DatetimeRange().setName(name).setLabel(label).setCallback(callback);
+    }
+
     public static Dependency dependency(String name) {
         return new Dependency().setName(name);
     }
 
     public static Dependency dependency(String name, String label) {
         return new Dependency().setName(name).setLabel(label);
+    }
+    
+    public static Dependency dependency(String name, String label, Closure callback) {
+        return new Dependency().setName(name).setLabel(label).setCallback(callback);
     }
 
     public static Display display(String name) {
@@ -116,12 +148,20 @@ public class Field {
         return new Display().setName(name).setLabel(label);
     }
 
+    public static Display display(String name, String label, Closure callback) {
+        return new Display().setName(name).setLabel(label).setCallback(callback);
+    }
+
     public static Editor editor(String name) {
         return new Editor().setName(name);
     }
 
     public static Editor editor(String name, String label) {
         return new Editor().setName(name).setLabel(label);
+    }
+
+    public static Editor editor(String name, String label, Closure callback) {
+        return new Editor().setName(name).setLabel(label).setCallback(callback);
     }
 
     public static Fieldset fieldset(String name) {
@@ -132,12 +172,20 @@ public class Field {
         return new Fieldset().setName(name).setLabel(label);
     }
 
+    public static Fieldset fieldset(String name, String label, Closure callback) {
+        return new Fieldset().setName(name).setLabel(label).setCallback(callback);
+    }
+
     public static File file(String name) {
         return new File().setName(name);
     }
 
     public static File file(String name, String label) {
         return new File().setName(name).setLabel(label);
+    }
+
+    public static File file(String name, String label, Closure callback) {
+        return new File().setName(name).setLabel(label).setCallback(callback);
     }
 
     public static Geofence geofence(String name) {
@@ -148,12 +196,20 @@ public class Field {
         return new Geofence().setName(name).setLabel(label);
     }
 
+    public static Geofence geofence(String name, String label, Closure callback) {
+        return new Geofence().setName(name).setLabel(label).setCallback(callback);
+    }
+
     public static Group group(String name) {
         return new Group().setName(name);
     }
 
     public static Group group(String name, String label) {
         return new Group().setName(name).setLabel(label);
+    }
+
+    public static Group group(String name, String label, Closure callback) {
+        return new Group().setName(name).setLabel(label).setCallback(callback);
     }
 
     public static Hidden hidden(String name) {
@@ -164,12 +220,20 @@ public class Field {
         return new Hidden().setName(name).setLabel(label);
     }
 
+    public static Hidden hidden(String name, String label, Closure callback) {
+        return new Hidden().setName(name).setLabel(label).setCallback(callback);
+    }
+
     public static Icon icon(String name) {
         return new Icon().setName(name);
     }
 
     public static Icon icon(String name, String label) {
         return new Icon().setName(name).setLabel(label);
+    }
+
+    public static Icon icon(String name, String label, Closure callback) {
+        return new Icon().setName(name).setLabel(label).setCallback(callback);
     }
 
     public static Id id(String name) {
@@ -180,12 +244,20 @@ public class Field {
         return new Id().setName(name).setLabel(label);
     }
 
+    public static Id id(String name, String label, Closure callback) {
+        return new Id().setName(name).setLabel(label).setCallback(callback);
+    }
+
     public static Image image(String name) {
         return new Image().setName(name);
     }
 
     public static Image image(String name, String label) {
         return new Image().setName(name).setLabel(label);
+    }
+
+    public static Image image(String name, String label, Closure callback) {
+        return new Image().setName(name).setLabel(label).setCallback(callback);
     }
 
     public static ImageCaptcha imageCaptcha(String name) {
@@ -196,12 +268,20 @@ public class Field {
         return new ImageCaptcha().setName(name).setLabel(label);
     }
 
+    public static ImageCaptcha imageCaptcha(String name, String label, Closure callback) {
+        return new ImageCaptcha().setName(name).setLabel(label).setCallback(callback);
+    }
+
     public static List list(String name) {
         return new List().setName(name);
     }
 
     public static List list(String name, String label) {
         return new List().setName(name).setLabel(label);
+    }
+
+    public static List list(String name, String label, Closure callback) {
+        return new List().setName(name).setLabel(label).setCallback(callback);
     }
 
     public static MapField mapField(String name) {
@@ -212,12 +292,20 @@ public class Field {
         return new MapField().setName(name).setLabel(label);
     }
 
+    public static MapField mapField(String name, String label, Closure callback) {
+        return new MapField().setName(name).setLabel(label).setCallback(callback);
+    }
+
     public static Month month(String name) {
         return new Month().setName(name);
     }
 
     public static Month month(String name, String label) {
         return new Month().setName(name).setLabel(label);
+    }
+
+    public static Month month(String name, String label, Closure callback) {
+        return new Month().setName(name).setLabel(label).setCallback(callback);
     }
 
     public static Number number(String name) {
@@ -228,12 +316,20 @@ public class Field {
         return new Number().setName(name).setLabel(label);
     }
 
+    public static Number number(String name, String label, Closure callback) {
+        return new Number().setName(name).setLabel(label).setCallback(callback);
+    }
+
     public static Password password(String name) {
         return new Password().setName(name);
     }
 
     public static Password password(String name, String label) {
         return new Password().setName(name).setLabel(label);
+    }
+
+    public static Password password(String name, String label, Closure callback) {
+        return new Password().setName(name).setLabel(label).setCallback(callback);
     }
 
     public static Quarter quarter(String name) {
@@ -244,12 +340,20 @@ public class Field {
         return new Quarter().setName(name).setLabel(label);
     }
 
+    public static Quarter quarter(String name, String label, Closure callback) {
+        return new Quarter().setName(name).setLabel(label).setCallback(callback);
+    }
+
     public static Radio radio(String name) {
         return new Radio().setName(name);
     }
 
     public static Radio radio(String name, String label) {
         return new Radio().setName(name).setLabel(label);
+    }
+
+    public static Radio radio(String name, String label, Closure callback) {
+        return new Radio().setName(name).setLabel(label).setCallback(callback);
     }
 
     public static Search search(String name) {
@@ -260,12 +364,20 @@ public class Field {
         return new Search().setName(name).setLabel(label);
     }
 
+    public static Search search(String name, String label, Closure callback) {
+        return new Search().setName(name).setLabel(label).setCallback(callback);
+    }
+
     public static SelectField select(String name) {
         return new SelectField().setName(name);
     }
 
     public static SelectField select(String name, String label) {
         return new SelectField().setName(name).setLabel(label);
+    }
+
+    public static SelectField select(String name, String label, Closure callback) {
+        return new SelectField().setName(name).setLabel(label).setCallback(callback);
     }
 
     public static SelectField selectField(String name) {
@@ -276,12 +388,20 @@ public class Field {
         return new SelectField().setName(name).setLabel(label);
     }
 
+    public static SelectField selectField(String name, String label, Closure callback) {
+        return new SelectField().setName(name).setLabel(label).setCallback(callback);
+    }
+
     public static Selects selects(String name) {
         return new Selects().setName(name);
     }
 
     public static Selects selects(String name, String label) {
         return new Selects().setName(name).setLabel(label);
+    }
+
+    public static Selects selects(String name, String label, Closure callback) {
+        return new Selects().setName(name).setLabel(label).setCallback(callback);
     }
 
     public static SmsCaptcha smsCaptcha(String name) {
@@ -292,6 +412,10 @@ public class Field {
         return new SmsCaptcha().setName(name).setLabel(label);
     }
 
+    public static SmsCaptcha smsCaptcha(String name, String label, Closure callback) {
+        return new SmsCaptcha().setName(name).setLabel(label).setCallback(callback);
+    }
+
     public static Space space(String name) {
         return new Space().setName(name);
     }
@@ -300,12 +424,20 @@ public class Field {
         return new Space().setName(name).setLabel(label);
     }
 
+    public static Space space(String name, String label, Closure callback) {
+        return new Space().setName(name).setLabel(label).setCallback(callback);
+    }
+
     public static SwitchField switchField(String name) {
         return new SwitchField().setName(name);
     }
 
     public static SwitchField switchField(String name, String label) {
         return new SwitchField().setName(name).setLabel(label);
+    }
+
+    public static SwitchField switchField(String name, String label, Closure callback) {
+        return new SwitchField().setName(name).setLabel(label).setCallback(callback);
     }
 
     public static Text text(String name) {
@@ -328,12 +460,20 @@ public class Field {
         return new Textarea().setName(name).setLabel(label);
     }
 
+    public static Textarea textarea(String name, String label, Closure callback) {
+        return new Textarea().setName(name).setLabel(label).setCallback(callback);
+    }
+
     public static Time time(String name) {
         return new Time().setName(name);
     }
 
     public static Time time(String name, String label) {
         return new Time().setName(name).setLabel(label);
+    }
+
+    public static Time time(String name, String label, Closure callback) {
+        return new Time().setName(name).setLabel(label).setCallback(callback);
     }
 
     public static TimeRange timeRange(String name) {
@@ -344,6 +484,10 @@ public class Field {
         return new TimeRange().setName(name).setLabel(label);
     }
 
+    public static TimeRange timeRange(String name, String label, Closure callback) {
+        return new TimeRange().setName(name).setLabel(label).setCallback(callback);
+    }
+
     public static Transfer transfer(String name) {
         return new Transfer().setName(name);
     }
@@ -352,12 +496,20 @@ public class Field {
         return new Transfer().setName(name).setLabel(label);
     }
 
+    public static Transfer transfer(String name, String label, Closure callback) {
+        return new Transfer().setName(name).setLabel(label).setCallback(callback);
+    }
+
     public static Tree tree(String name) {
         return new Tree().setName(name);
     }
 
     public static Tree tree(String name, String label) {
         return new Tree().setName(name).setLabel(label);
+    }
+    
+    public static Tree tree(String name, String label, Closure callback) {
+        return new Tree().setName(name).setLabel(label).setCallback(callback);
     }
 
     public static TreeSelect treeSelect(String name) {
@@ -368,6 +520,10 @@ public class Field {
         return new TreeSelect().setName(name).setLabel(label);
     }
 
+    public static TreeSelect treeSelect(String name, String label, Closure callback) {
+        return new TreeSelect().setName(name).setLabel(label).setCallback(callback);
+    }
+
     public static Week week(String name) {
         return new Week().setName(name);
     }
@@ -376,11 +532,19 @@ public class Field {
         return new Week().setName(name).setLabel(label);
     }
 
+    public static Week week(String name, String label, Closure callback) {
+        return new Week().setName(name).setLabel(label).setCallback(callback);
+    }
+
     public static Year year(String name) {
         return new Year().setName(name);
     }
 
     public static Year year(String name, String label) {
         return new Year().setName(name).setLabel(label);
+    }
+
+    public static Year year(String name, String label, Closure callback) {
+        return new Year().setName(name).setLabel(label).setCallback(callback);
     }
 }
