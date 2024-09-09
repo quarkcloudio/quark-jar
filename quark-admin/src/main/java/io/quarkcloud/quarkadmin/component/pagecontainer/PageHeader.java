5332b135cc50fea3617294d63499065d9c740198
tangtanglove <dai_hang_love@126.com>
2024-09-09 14:51:18 +0800

    chore: 修正代码

--- quark-admin/src/main/java/io/quarkcloud/quarkadmin/component/pagecontainer/PageHeader.java
+++ quark-admin/src/main/java/io/quarkcloud/quarkadmin/component/pagecontainer/PageHeader.java
@@ -2,6 +2,8 @@ package io.quarkcloud.quarkadmin.component.pagecontainer;
 
 import java.util.HashMap;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import io.quarkcloud.quarkadmin.component.Component;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -15,31 +17,37 @@ public class PageHeader extends Component  {
     /**
      * 标题栏旁的头像
      */
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
     public Object avatar;
 
     /**
      * 自定义 back icon，如果为 false 不渲染 back icon
      */
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
     public Object backIcon;
 
     /**
      * 面包屑的配置
      */
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
     public Object breadcrumb;
 
     /**
      * 自定义面包屑区域的内容
      */
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
     public Object breadcrumbRender;
 
     /**
      * 操作区，位于 title 行的行尾
      */
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
     public Object extra;
 
     /**
      * PageHeader 的页脚，一般用于渲染 TabBar
      */
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
     public Object footer;
 
     /**
