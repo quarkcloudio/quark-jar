d77b016baa9b5c5b61574249654952b51677eb45
tangtanglove <dai_hang_love@126.com>
2024-09-13 17:22:45 +0800

    chore: wip

--- quark-starter/src/main/java/io/quarkcloud/quarkstarter/service/admin/action/ChangeAccount.java
+++ quark-starter/src/main/java/io/quarkcloud/quarkstarter/service/admin/action/ChangeAccount.java
@@ -1,6 +1,5 @@
 package io.quarkcloud.quarkstarter.service.admin.action;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
@@ -11,18 +10,11 @@ import cn.hutool.jwt.JWT;
 import io.quarkcloud.quarkadmin.component.message.Message;
 import io.quarkcloud.quarkadmin.entity.AdminEntity;
 import io.quarkcloud.quarkadmin.mapper.ResourceMapper;
-import io.quarkcloud.quarkadmin.service.AdminService;
 import io.quarkcloud.quarkadmin.service.ResourceService;
 import io.quarkcloud.quarkadmin.template.resource.impl.action.AjaxImpl;
 import io.quarkcloud.quarkcore.service.Context;
 
 public class ChangeAccount<M, T> extends AjaxImpl<ResourceMapper<T>, T> {
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @Autowired
-    private AdminService adminService;
 
     // 执行行为句柄
     public Object handle(Context context, UpdateWrapper<T> updateWrapper, ResourceService<ResourceMapper<T>, T> resourceService) {
@@ -30,6 +22,7 @@ public class ChangeAccount<M, T> extends AjaxImpl<ResourceMapper<T>, T> {
         AdminEntity adminEntity = context.getRequestBody(AdminEntity.class);
         Long adminId = Long.parseLong(jwt.getPayload("id").toString());
         if (adminEntity.getAvatar() != null) {
+            ObjectMapper objectMapper = new ObjectMapper();
             try {
                 adminEntity.setAvatar(objectMapper.writeValueAsString(adminEntity.getAvatar()));
             } catch (JsonProcessingException e) {
@@ -37,13 +30,13 @@ public class ChangeAccount<M, T> extends AjaxImpl<ResourceMapper<T>, T> {
             }
         }
 
-        if (!adminEntity.getPassword().isEmpty()) {
+        if (adminEntity.getPassword() !=null && !adminEntity.getPassword().isEmpty()) {
             BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
             adminEntity.setPassword(encoder.encode(adminEntity.getPassword()));
         }
 
         // 获取登录管理员信息
-        AdminEntity adminInfo = adminService.getById(adminId);
+        T adminInfo = resourceService.getById(adminId);
         if (adminInfo == null) {
             return Message.error("管理员信息获取失败");
         }
@@ -51,7 +44,7 @@ public class ChangeAccount<M, T> extends AjaxImpl<ResourceMapper<T>, T> {
         adminEntity.setId(adminId);
 
         // 更新管理员信息
-        boolean result = adminService.updateById(adminEntity);
+        boolean result = resourceService.updateById((T) adminEntity);
         if (!result) {
             return Message.error("操作失败！");
         }
