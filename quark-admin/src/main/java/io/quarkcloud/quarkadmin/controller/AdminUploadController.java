21b961f6a9e6ae98f59929bc6378857792577bc5
tangtanglove <dai_hang_love@126.com>
2024-09-18 14:59:46 +0800

    feat: 完善文件上传

--- quark-admin/src/main/java/io/quarkcloud/quarkadmin/controller/AdminUploadController.java
+++ quark-admin/src/main/java/io/quarkcloud/quarkadmin/controller/AdminUploadController.java
@@ -7,12 +7,14 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Base64;
 import java.util.List;
 import java.util.Map;
 
 import javax.imageio.ImageIO;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -37,6 +39,7 @@ import io.quarkcloud.quarkadmin.entity.FileEntity;
 import io.quarkcloud.quarkadmin.entity.PictureCategoryEntity;
 import io.quarkcloud.quarkadmin.service.PictureService;
 import io.quarkcloud.quarkcore.service.Env;
+import jakarta.servlet.http.HttpServletRequest;
 import io.quarkcloud.quarkadmin.service.FileService;
 import io.quarkcloud.quarkadmin.service.PictureCategoryService;
 
@@ -58,12 +61,12 @@ public class AdminUploadController {
     @RequestMapping(value = "/api/admin/upload/image/getList", method = {RequestMethod.GET})
     @ResponseBody
     public Object getImageList(
-        @RequestParam(value = "page", defaultValue = "1") Integer page,
-        @RequestParam(value = "pictureCategoryId", defaultValue = "") Integer categoryId,
-        @RequestParam(value = "pictureSearchName", defaultValue = "") String name,
-        @RequestParam(value = "pictureSearchDate", defaultValue = "") String[] date,
-        @RequestHeader(value = "Authorization", defaultValue = "") String authorization
-    ) {
+            @RequestParam(value = "page", defaultValue = "1") Integer page,
+            @RequestParam(value = "pictureCategoryId", defaultValue = "") Integer categoryId,
+            @RequestParam(value = "pictureSearchName", defaultValue = "") String name,
+            @RequestParam(value = "pictureSearchDate", defaultValue = "") String[] date,
+            @RequestHeader(value = "Authorization", defaultValue = "") String authorization
+        ) {
         if (authorization == null || authorization.isEmpty() || !authorization.startsWith("Bearer ")) {
             throw new RuntimeException("参数错误");
         }
@@ -107,33 +110,143 @@ public class AdminUploadController {
 
     @RequestMapping(value = "/api/admin/upload/image/crop", method = {RequestMethod.POST})
     @ResponseBody
-    public Object imageCrop(@RequestParam("file") MultipartFile file) {
-        return "";
-    }
+    public Object imageCrop(
+            @RequestParam(value = "limitW", required = false) String limitW,
+            @RequestParam(value = "limitH", required = false) String limitH,
+            @RequestBody Map<String, Object> data,
+            @RequestHeader(value = "Authorization", defaultValue = "") String authorization,
+            HttpServletRequest request) {
 
-    @RequestMapping(value = "/api/admin/upload/image/handle", method = {RequestMethod.POST})
-    @ResponseBody
-    public Object imageHandle(@RequestParam("file") MultipartFile file, @RequestHeader(value = "Authorization", defaultValue = "") String authorization) throws IOException, NoSuchAlgorithmException {
-        byte[] fileBytes = file.getBytes();
-        String fileName = file.getOriginalFilename();
+        String id = (String) data.get("id");
+        String file = (String) data.get("file");
+        if (id == null || file == null) {
+            return Message.error("参数错误！");
+        }
 
-        // 文件不存在或文件名为空
-        if (fileBytes == null || StrUtil.isBlank(fileName)) {
-            throw new RuntimeException("文件不存在或文件名为空");
+        PictureEntity pictureInfo = pictureService.getById(id);
+        if (pictureInfo == null || pictureInfo.getId() == 0) {
+            return Message.error("文件不存在");
         }
 
         if (authorization == null || authorization.isEmpty() || !authorization.startsWith("Bearer ")) {
-            throw new RuntimeException("参数错误");
+            return Message.error("参数错误！");
         }
         String token = authorization.substring("Bearer ".length());
         String appKey = Env.getProperty("app.key");
         if (!JWT.of(token).setKey(appKey.getBytes()).verify()) {
-            throw new RuntimeException("无权操作");
+            return Message.error("无权操作");
         }
         try {
             JWTValidator.of(token).validateDate(DateUtil.date());
         } catch (Exception e) {
-            throw new RuntimeException("认证已过期");
+            return Message.error("认证已过期");
+        }
+        final JWT jwt = JWTUtil.parseToken(token);
+        if (jwt == null) {
+            return Message.error("认证失败");
+        }
+
+        String[] files = file.split(",");
+        if (files.length != 2) {
+            return Message.error("格式错误");
+        }
+
+        byte[] fileData;
+        try {
+            fileData = Base64.getDecoder().decode(files[1]);
+        } catch (IllegalArgumentException e) {
+            return Message.error("文件数据解码失败");
+        }
+
+        File uploadFile = FileUtil.file(pictureInfo.getPath());
+
+        // 将文件写入服务器指定目录
+        FileUtil.writeBytes(fileData, uploadFile);
+ 
+        // 文件大小
+        Long fileSize = uploadFile.length();
+        
+        // 文件hash
+        MessageDigest digest;
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            return Message.error(e.getMessage());
+        }
+        byte[] hashBytes = digest.digest(fileData);
+        StringBuilder sb = new StringBuilder();
+        for (byte b : hashBytes) {
+            sb.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
+        }
+        String fileHash = sb.toString();
+
+        // 读取图片
+        BufferedImage image;
+        try {
+            image = ImageIO.read(uploadFile);
+        } catch (IOException e) {
+            return Message.error(e.getMessage());
+        }
+        
+        // 获取图片宽度和高度
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        pictureInfo.setObjType("ADMINID");
+        pictureInfo.setObjId((Long) jwt.getPayload("id"));
+        pictureInfo.setSize(fileSize);
+        pictureInfo.setHash(fileHash);
+        pictureInfo.setHeight(height);
+        pictureInfo.setWidth(width);
+
+        boolean result = pictureService.updateById(pictureInfo);
+        if (!result) {
+            return Message.error("操作失败，请重试！");
+        }
+
+        // 返回上传成功的消息
+        return Message.success("操作成功", "", Map.of(
+            "id", pictureInfo.getId(),
+            "ext", pictureInfo.getExt(),
+            "hash", pictureInfo.getHash(),
+            "height", pictureInfo.getHeight(),
+            "width", pictureInfo.getWidth(),
+            "name", pictureInfo.getName(),
+            "path", pictureInfo.getPath(),
+            "size", pictureInfo.getSize(),
+            "url", pictureInfo.getUrl()
+        ));
+    }
+
+    @RequestMapping(value = "/api/admin/upload/image/handle", method = {RequestMethod.POST})
+    @ResponseBody
+    public Object imageHandle(@RequestParam("file") MultipartFile file, @RequestHeader(value = "Authorization", defaultValue = "") String authorization) {
+        byte[] fileBytes;
+        try {
+            fileBytes = file.getBytes();
+        } catch (IOException e) {
+            return Message.error(e.getMessage());
+        }
+        
+        String fileName = file.getOriginalFilename();
+
+        // 文件不存在或文件名为空
+        if (fileBytes == null || StrUtil.isBlank(fileName)) {
+            return Message.error("文件不存在或文件名为空");
+        }
+
+        if (authorization == null || authorization.isEmpty() || !authorization.startsWith("Bearer ")) {
+            return Message.error("参数错误");
+        }
+        String token = authorization.substring("Bearer ".length());
+        String appKey = Env.getProperty("app.key");
+        if (!JWT.of(token).setKey(appKey.getBytes()).verify()) {
+            return Message.error("无权操作");
+        }
+        try {
+            JWTValidator.of(token).validateDate(DateUtil.date());
+        } catch (Exception e) {
+            return Message.error("认证已过期");
         }
         final JWT jwt = JWTUtil.parseToken(token);
  
@@ -168,7 +281,12 @@ public class AdminUploadController {
         Long fileSize = uploadFile.length();
         
         // 文件hash
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        MessageDigest digest;
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            return Message.error(e.getMessage());
+        }
         byte[] hashBytes = digest.digest(fileBytes);
         StringBuilder sb = new StringBuilder();
         for (byte b : hashBytes) {
@@ -177,7 +295,12 @@ public class AdminUploadController {
         String fileHash = sb.toString();
 
         // 读取图片
-        BufferedImage image = ImageIO.read(uploadFile);
+        BufferedImage image;
+        try {
+            image = ImageIO.read(uploadFile);
+        } catch (IOException e) {
+            return Message.error(e.getMessage());
+        }
         
         // 获取图片宽度和高度
         int width = image.getWidth();
@@ -223,8 +346,13 @@ public class AdminUploadController {
 
     @RequestMapping(value = "/api/admin/upload/file/handle", method = {RequestMethod.POST})
     @ResponseBody
-    public Object fileHandle(@RequestParam("file") MultipartFile file, @RequestHeader(value = "Authorization", defaultValue = "") String authorization) throws IOException, NoSuchAlgorithmException {
-        byte[] fileBytes = file.getBytes();
+    public Object fileHandle(@RequestParam("file") MultipartFile file, @RequestHeader(value = "Authorization", defaultValue = "") String authorization) {
+        byte[] fileBytes;
+        try {
+            fileBytes = file.getBytes();
+        } catch (IOException e) {
+            return Message.error(e.getMessage());
+        }
         String fileName = file.getOriginalFilename();
 
         // 文件不存在或文件名为空
@@ -278,7 +406,12 @@ public class AdminUploadController {
         Long fileSize = uploadFile.length();
         
         // 文件hash
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        MessageDigest digest;
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            return Message.error(e.getMessage());
+        }
         byte[] hashBytes = digest.digest(fileBytes);
         StringBuilder sb = new StringBuilder();
         for (byte b : hashBytes) {
