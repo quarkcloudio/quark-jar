e90bebb5cb192b3d1239833b365fd27d65cf69fb
tangtanglove <dai_hang_love@126.com>
2024-10-30 09:45:05 +0800

    chore: wip

--- quark-starter/src/main/java/io/quarkcloud/quarkstarter/controller/IndexController.java
+++ quark-starter/src/main/java/io/quarkcloud/quarkstarter/controller/IndexController.java
@@ -1,15 +1,17 @@
 package io.quarkcloud.quarkstarter.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping("/")
-    @ResponseBody
-    public String index() {
-        return "Hello World!";
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("message", "Hello World!");
+        return "index";
     }
 }
