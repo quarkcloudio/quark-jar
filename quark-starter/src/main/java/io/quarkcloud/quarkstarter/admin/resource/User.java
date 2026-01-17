3788865fb838460e70ea1c860b48ea707aff47bb
tangtanglove <dai_hang_love@126.com>
2026-01-17 14:57:28 +0800

    chore: 优化代码

--- quark-starter/src/main/java/io/quarkcloud/quarkstarter/admin/resource/User.java
+++ quark-starter/src/main/java/io/quarkcloud/quarkstarter/admin/resource/User.java
@@ -109,9 +109,7 @@ public class User extends ResourceImpl<UserMapper, UserEntity> {
             Field.id("id", "ID"),
             Field.image("avatar", "头像")
                 .onlyOnForms(),
-            Field.text("username", "用户名", () -> {
-                    return String.format("<a href='#/layout/index?api=/api/admin/user/edit&id=%d'>%s</a>", this.entity.getId(), this.entity.getUsername());
-                })
+            Field.text("username", "用户名")
                 .setRules(Arrays.asList(
                     Rule.required("用户名必须填写"),
                     Rule.min(6, "用户名不能少于6个字符"),
