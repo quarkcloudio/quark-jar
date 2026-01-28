9dc59cf35cd4f30d2cc840626a7a8c7fb3d8bfd9
tangtanglove <dai_hang_love@126.com>
2026-01-28 22:30:28 +0800

    chore: 优化代码

--- quark-admin/src/main/java/io/quarkcloud/quarkadmin/template/dashboard/Dashboard.java
+++ quark-admin/src/main/java/io/quarkcloud/quarkadmin/template/dashboard/Dashboard.java
@@ -5,24 +5,8 @@ import io.quarkcloud.quarkadmin.template.metrics.Metrics;
 import io.quarkcloud.quarkcore.service.Context;
 
 public interface Dashboard {
-
-    // 获取标题
-    public String getTitle();
-
-    // 获取页面子标题
-    public String getSubTitle();
-
-    // 获取页面是否携带返回Icon
-    public boolean isBackIcon();
-
     // 内容
     public List<Metrics> cards(Context context);
-
-    // 页面组件渲染
-    public Object pageComponentRender(Context context, Object body);
-
-    // 页面容器组件渲染
-    public Object pageContainerComponentRender(Context context, Object body);
 
     // 组件渲染
     public Object render(Context context);
--- quark-admin/src/main/java/io/quarkcloud/quarkadmin/template/dashboard/impl/DashboardImpl.java
+++ quark-admin/src/main/java/io/quarkcloud/quarkadmin/template/dashboard/impl/DashboardImpl.java
@@ -6,28 +6,18 @@ import java.util.List;
 import java.util.Map;
 
 import io.quarkcloud.quarkadmin.annotation.AdminDashboard;
-import io.quarkcloud.quarkadmin.component.pagecontainer.PageContainer;
-import io.quarkcloud.quarkadmin.component.pagecontainer.PageHeader;
 import io.quarkcloud.quarkadmin.component.card.Card;
 import io.quarkcloud.quarkadmin.component.grid.Col;
 import io.quarkcloud.quarkadmin.component.grid.Row;
 import io.quarkcloud.quarkadmin.template.dashboard.Dashboard;
 import io.quarkcloud.quarkadmin.template.metrics.Metrics;
+import io.quarkcloud.quarkcore.common.Message;
 import io.quarkcloud.quarkcore.service.Context;
 
 public class DashboardImpl implements Dashboard {
 
     // 注解实例
     protected AdminDashboard annotationClass = null;
-
-    // 页面标题
-    public String title;
-
-    // 页面子标题
-    public String subTitle;
-
-    // 页面是否携带返回Icon
-	public boolean backIcon;
 
     // 构造函数
     public DashboardImpl() {
@@ -36,93 +26,11 @@ public class DashboardImpl implements Dashboard {
         if (getClass().isAnnotationPresent(AdminDashboard.class)) {
             annotationClass = getClass().getAnnotation(AdminDashboard.class);
         }
-    
-        // 页面标题
-        title = "仪表盘";
-    }
-
-    // 获取标题
-    public String getTitle() {
-
-        // 检查是否存在注解
-        if (annotationClass == null) {
-            return title;
-        }
-
-        // 注解值为空返回默认值
-        if (annotationClass.title().isEmpty()) {
-            return title;
-        }
-
-        // 获取注解值
-        return annotationClass.title();
-    }
-
-    // 获取页面子标题
-    public String getSubTitle() {
-
-        // 检查是否存在注解
-        if (annotationClass == null) {
-            return subTitle;
-        }
-
-        // 注解值为空返回默认值
-        if (annotationClass.subTitle().isEmpty()) {
-            return subTitle;
-        }
-
-        // 获取注解值
-        return annotationClass.subTitle();
-    }
-
-    // 获取页面是否携带返回Icon
-    public boolean isBackIcon() {
-
-        // 检查是否存在注解
-        if (annotationClass == null) {
-            return backIcon;
-        }
-
-        // 获取注解值
-        return annotationClass.backIcon();
     }
 
     // 内容
     public List<Metrics> cards(Context context) {
         return null;
-    }
-
-    // 页面组件渲染
-    public Object pageComponentRender(Context context, Object body) {
-
-        // 页面容器组件渲染
-        return this.pageContainerComponentRender(context, body);
-    }
-
-    // 页面容器组件渲染
-    public Object pageContainerComponentRender(Context context, Object body) {
-
-        // 页面标题
-        String title = this.getTitle();
-
-        // 页面子标题
-        String subTitle = this.getSubTitle();
-
-        // 页面是否携带返回Icon
-        boolean backIcon = this.isBackIcon();
-
-        // 设置头部
-        PageHeader header = new PageHeader()
-                .setTitle(title)
-                .setSubTitle(subTitle);
-
-        if (!backIcon) {
-            header.setBackIcon(false);
-        }
-
-        return new PageContainer()
-                .setHeader(header)
-                .setBody(body);
     }
 
     // 组件渲染
@@ -168,6 +76,6 @@ public class DashboardImpl implements Dashboard {
             body.add(row);
         }
 
-        return this.pageComponentRender(context, body);
+        return Message.success("ok", body);
     }
 }
